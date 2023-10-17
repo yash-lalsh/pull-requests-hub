b0048b88b4d63514b64a9d8bca0246e7fc6d0b38
Yash <yashvardhan312@gmail.com>
2023-10-17 22:09:43 +0100

    grid width change

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         </div>
       ) : (
         <div className="w-full flex items-center justify-center">
-          <div className="w-full grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 max-w-[1000px] 2xl:max-w-[1440px] gap-8 py-16 px-8 sm:px-16">
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 max-w-[1100px] 2xl:max-w-[1440px] gap-8 py-16 px-8 sm:px-16">
             {pullRequestsData.map((pr) => {
               return <PullRequestCard key={pr.number} {...pr} />;
             })}
